423cbecef735cd4e64134174b5d49525666cccc8
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-07-26 15:13:18 +0200

    openning commit

---
+++ Java11Exercises/src/main/java/Exercises/ShoppingCart.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercises;
+
+/**
+ *
+ * @author jaden
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+       String[] custName = {"Mary Smith", "Trystan Dames", "Jaden Turnbell"};
+        String[] items = {"Shirt", "Belt", "Skirt", "Cap"};
+        String message;
+
+       double price = 21.99;
+       int quantity = 2;
+       double total;
+       message = custName[2]+" wants to purchase "+ items.length + " items";
+       System.out.println(message);
+
+        System.out.println("Items purchased: ");
+
+        for (String item : items){
+            System.out.print(item + ", ");
+        }
+       
+       
+    }
+}
